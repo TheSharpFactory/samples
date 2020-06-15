4cb034c6c6fd5d77c15e4888f62bf2c645ec7cd9
Rafsanul Hasan <rafsanulhasan@outlook.com>
2020-06-15 08:56:10 +0600

    Updated Shared ViewModel: 1. Set C# langversion to 7.3 2. Added a ViewModel for Web

---
+++ sources/Applications/SharedViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheSharpFactory.Apps.Shared.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ sources/Applications/SharedViewModels/Web/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheSharpFactory.Apps.Shared.ViewModels.Web
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
