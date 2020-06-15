4ace6bab6c7c2aed90503ee8c891d25915219594
Rafsanul Hasan <rafsanulhasan@outlook.com>
2020-06-15 09:16:37 +0600

    Moved Shared UI for Blazor to a new forlder

---
+++ sources/Applications/Web/SharedUI.Blazor/ArgumentAttributes.cs
@@ -0,0 +1,8 @@
+﻿namespace TheSharpFactory.Apps.Web.SharedUI
+{
+    public enum ArgumentAttributes
+    {
+        Flavor,
+        Platform
+    }
+}
---
+++ sources/Applications/Web/SharedUI.Blazor/BlazorFlavor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TheSharpFactory.Apps.Web.SharedUI
+{
+    [Flags]
+    public enum BlazorFlavor
+    {
+        Server,
+        WebAssembly,
+        PWA,
+        Hybrid,
+        Native
+    }
+}
