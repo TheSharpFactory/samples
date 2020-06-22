bfb498c7cd94375ec7b33722cc120dcdc2cff286
Rafsanul Hasan <rafsanulhasan@outlook.com>
2020-06-22 19:27:30 +0600

    Completed AB#16

---
+++ sources/Domain/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace TheSharpFactory.Domain.Models
+{
+    public class Customer
+        : ICustomer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public ICustomerProfile Profile { get; set; }
+    }
+}
