87fa6243d0cfc981a5bff0e5d61eafe6a16c5c95
Rafsanul Hasan <rafsanulhasan@outlook.com>
2020-06-23 07:08:26 +0600

    Completed AB#13: Created Extensions for DI

---
+++ sources/Common/Extentions.DependencyInjection/Mappers/MapperExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+using AutoMapper;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using TheSharpFactory.Common.DTO;
+using TheSharpFactory.Data.Entity.WorldWideImporters.Sales;
+
+namespace TheSharpFactory.Common.Extensions.DependencyInjection.Mappers
+{
+    public static class MapperExtensions
+    {
+        private static readonly Assembly[] ScannableAssemblies =
+            new Assembly[]
+            {
+                typeof(Customer).Assembly,
+                typeof(CustomerDTO).Assembly
+            };
+        public static IServiceCollection AddMapper(this IServiceCollection services)
+            => services.AddAutoMapper(ScannableAssemblies);
+    }
+}
