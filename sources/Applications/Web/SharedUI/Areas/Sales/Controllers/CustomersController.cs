3c424b08191406ff5e94d425d8061d75c3c1076f
Rafsanul Hasan <rafsanulhasan@outlook.com>
2020-07-04 07:40:32 +0600

    Configured MVC views and controllers

---
+++ sources/Applications/Web/SharedUI/Areas/Sales/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+using HotChocolate.AspNetCore;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using TheSharpFactory.Apps.Shared.ViewModels.Conventional;
+
+namespace TheSharpFactory.Apps.Web.SharedUI.Areas.Landing.Controllers
+{
+    [Area("Sales")]
+    [Route("[area]/[controller]/{action}")]
+    public class CustomersController
+        : Controller
+    {
+        private readonly ILogger<CustomersController> _logger;
+        private readonly ICustomersViewModel _customer;
+
+        public CustomersController(
+            ILogger<CustomersController> logger,
+            ICustomersViewModel customer
+        )
+        {
+            _logger = logger;
+            _customer = customer;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            await _customer.GetCustomers(SDK.HttpServiceTypes.REST).ConfigureAwait(true);
+            return View(_customer.AllCustomers);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+            => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
--- sources/Applications/Web/SharedUI/StartupBase.cs
+++ sources/Applications/Web/SharedUI/StartupBase.cs
@@ -1,15 +1,16 @@
 ﻿
-using System;
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 using TheSharpFactory.Apps.Shared.Services;
+using TheSharpFactory.Apps.Shared.ViewModels;
 using TheSharpFactory.Apps.Shared.ViewModels.Conventional;
+using TheSharpFactory.Common.Extensions.DependencyInjection.Mappers;
+using TheSharpFactory.SDK;
+using TheSharpFactory.SDK.Clients;
 
 namespace TheSharpFactory.Apps.Web.SharedUI
 {
@@ -24,21 +25,28 @@ namespace TheSharpFactory.Apps.Web.SharedUI
 
         public virtual void ConfigureServices(IServiceCollection services)
         {
-            if (AppModel.CommunicationWay == MvcCommunicationWay.Direct)
+            if (AppModel.CommunicationWay == MvcCommunicationWay.ViaAPI)
             {
-                //Database.RegisterModelConnectionString(
-                //    RepoLookup.ModelId.WWI,
-                //    Configuration.GetConnectionString("WWIConnectionString")
-                //);
+                services.AddMapper(
+                    false,
+                    false,
+                    typeof(DTOToViewModelMappingProfile).Assembly,
+                    typeof(CustomerMessageToDTOMappingProfile).Assembly
+                );
+                var apiConf = Configuration.GetSection("Api").Get<ApiConfiguration>();
+                services.AddSharpFactoryApiClients(apiConf.Url);
             }
 
             services.AddSingleton(_ => AppModel);
             services.AddTransient<IWeatherForecastDTO, WeatherForecastDTO>();
             services.AddTransient<IWeatherForecastService, WeatherForecastService>();
             services.AddTransient<IFetchDataViewModel, FetchDataViewModel>();
+            services.AddTransient<ICustomersViewModel, CustomersViewModel>();
             services.AddSingleton<ICounterService, CounterService>();
 
-            services.AddControllersWithViews(opt => opt.EnableEndpointRouting = false);
+            services.AddControllersWithViews(opt
+                => opt.EnableEndpointRouting = false
+            );
         }
 
         public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment env, MvcApplicationModel appModel)
@@ -76,6 +84,17 @@ namespace TheSharpFactory.Apps.Web.SharedUI
                     "areas",
                     "Landing",
                     "{area:Exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+                endpoints.MapAreaControllerRoute(
+                    "sales",
+                    "Sales",
+                    "{area:Exists}/{controller=Customers}/{action=Index}/{id?}"
+                );
+                endpoints.MapFallbackToAreaController(
+                    "{area:Exists}/{controller=Home}/{action=Index}/{id?}",
+                    "Index",
+                    "Home",
+                    "Landing"
                 );
                 endpoints.MapControllerRoute(
                     name: "default",
