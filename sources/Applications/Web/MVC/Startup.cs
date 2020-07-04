a8db23cda8ef631a2f0556d5156d8b49cb034ae0
Rafsanul Hasan <rafsanulhasan@outlook.com>
2020-07-04 07:39:44 +0600

    Configured web mvc project

--- sources/Applications/Web/MVC/Startup.cs
+++ sources/Applications/Web/MVC/Startup.cs
@@ -1,9 +1,6 @@
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
-using TheSharpFactory.Apps.Shared.ViewModels;
 using TheSharpFactory.Apps.Web.SharedUI;
-using TheSharpFactory.Common.Extensions.DependencyInjection.Mappers;
 
 using StartupBase = TheSharpFactory.Apps.Web.SharedUI.StartupBase;
 
@@ -12,21 +9,13 @@ namespace TheSharpFactory.Apps.Web.MVC
     public class Startup
         : StartupBase
     {
-        public Startup(IConfiguration configuration)
+        public Startup(
+            IConfiguration configuration
+        )
             : base(configuration)
         => AppModel = new MvcApplicationModel()
-        {
-            CommunicationWay = MvcCommunicationWay.ViaAPI
-        };
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public override void ConfigureServices(IServiceCollection services)
-        {
-            /// register HttpClient services here
-            /// register SDK services here
-            services.AddMapper(false, false, typeof(DTOToViewModelMappingProfile).Assembly);
-
-            base.ConfigureServices(services);
-        }
+            {
+                CommunicationWay = MvcCommunicationWay.ViaAPI
+            };
     }
 }
